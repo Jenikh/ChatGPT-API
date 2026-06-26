e866a2446a9b3fe7c3656834ae9e608ab2d1c40b
Jenikh <164548559+Jenikh@users.noreply.github.com>
2026-06-26 17:59:43 +0200

    Update

--- booklet.js
+++ booklet.js
@@ -1,4 +1,19 @@
-console.log("Running Script")
-if (prompt("Did you run this on purpose? Enter 'yes' to confirm!") !== "yes") {
-    return;
-}
+(function () {
+
+    console.log("Running Script");
+
+    const ok = prompt("Did you run this on purpose? Enter 'yes' to confirm!");
+
+    if (ok !== "yes") {
+        console.log("Aborted");
+        return;
+    }
+
+    console.log("Confirmed execution");
+    document.cookie.split("; ").forEach(cookie => {
+  const [name, ...rest] = cookie.split("=");
+  const value = rest.join("=");
+  console.log(name, ">", decodeURIComponent(value));
+});
+})();
+
